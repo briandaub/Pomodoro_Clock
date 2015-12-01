1822b341ba81e3239a5936ee430c02ae0e80ea7b
Brian <brian@rockridgecreative.com>
2015-11-30 22:27:46 -0600

    Canvas Function cont'd

--- js/script.js
+++ js/script.js
@@ -19,7 +19,9 @@ function setSessionLength(time){
 
 var canvas = document.getElementById('canvas');
 var ctx = canvas.getContext('2d');
-ctx.strokeStyle = '28d1fa';
+ctx.strokeStyle = '#28d1fa';
+ctx.lineWidth = 17;
+ctx.lineCap = 'round';
 
 function degToRad(degree){
     var factor = Math.PI/180;
@@ -34,23 +36,22 @@ function renderTimer(){
     var newSeconds = seconds+(milliseconds/1000);
     
     // Background
-    ctx.fillStyle = '333333';
-    ctx.fillRect(0,0,500,500);
-     
+    ctx.fillStyle = '#333333';
+    ctx.fillRect(0, 0, 500, 500);
     
     //hours
     ctx.beginPath();
-    ctx.arc(250, 250, 200, degToRad(270), degToRad((hours*15)-90));
+    ctx.arc(250, 250, 180, degToRad(270), degToRad((hours*15)-90));
     ctx.stroke();
     
     //minutes
     ctx.beginPath();
-    ctx.arc(250, 250, 170, degToRad(270), degToRad((minutes*6)-90));
+    ctx.arc(250, 250, 150, degToRad(270), degToRad((minutes*6)-90));
     ctx.stroke();
     
     //seconds
     ctx.beginPath();
-    ctx.arc(250, 250, 140, degToRad(270), degToRad(seconds*6)-90);
+    ctx.arc(250, 250, 120, degToRad(270), degToRad(seconds*6)-90);
     ctx.stroke();
     
 }

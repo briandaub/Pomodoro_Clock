d5bfba0afea00ed17a179f988e59edf2ad01c305
Brian <brian@rockridgecreative.com>
2015-11-30 20:21:57 -0600

    Canvas Function Added

--- js/script.js
+++ js/script.js
@@ -1,34 +1,3 @@
-var canvas = document.getElementById('canvas');
-var ctx = canvas.getContext('2d');
-
-function degToRad(degree){
-    var factor = Math.PI/180;
-    return degree*factor;
-}
-
-function renderTimer(){
-    var mil = 0;
-    var sec = 0; 
-    var min = 0; 
-    var hour = 0;  
-    
-    //hours
-    ctx.beginPath();
-    ctx.arc(250, 250, 200, 0, degToRad(180));
-    ctx.stroke();
-    
-    //minutes
-    ctx.beginPath();
-    ctx.arc(250, 250, 170, 0, degToRad(180));
-    ctx.stroke();
-    
-    //seconds
-    ctx.beginPath();
-    ctx.arc(250, 250, 140, 0, degToRad(180));
-    ctx.stroke();
-    
-}
-
 var breakLength = 5;
 var sessionLength = 25;
 
@@ -47,3 +16,35 @@ function setSessionLength(time){
     }
     document.getElementById('sessionTime').innerHTML = sessionLength;
 }
+
+var canvas = document.getElementById('canvas');
+var ctx = canvas.getContext('2d');
+
+function degToRad(degree){
+    var factor = Math.PI/180;
+    return degree*factor;
+}
+
+function renderTimer(){
+    var mil = 0;
+    var sec = 0; 
+    var min = 0; 
+    var hour = 0;  
+    
+    //hours
+    ctx.beginPath();
+    ctx.arc(250, 250, 200, degToRad, degToRad(180));
+    ctx.stroke();
+    
+    //minutes
+    ctx.beginPath();
+    ctx.arc(250, 250, 170, 0, degToRad(180));
+    ctx.stroke();
+    
+    //seconds
+    ctx.beginPath();
+    ctx.arc(250, 250, 140, 0, degToRad(180));
+    ctx.stroke();
+    
+}
+renderTimer();

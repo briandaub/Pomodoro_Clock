ec80f9947c5eed22e9f7e12c9309df7fadb6cc76
Brian <brian@rockridgecreative.com>
2015-11-30 19:50:44 -0600

    Added Canvas Function
    Added canvas function

--- js/script.js
+++ js/script.js
@@ -1,11 +1,31 @@
 var canvas = document.getElementById('canvas');
 var ctx = canvas.getContext('2d');
 
+function degToRad(degree){
+    var factor = Math.PI/180;
+    return degree*factor;
+}
+
 function renderTimer(){
     var mil = 0;
     var sec = 0; 
     var min = 0; 
     var hour = 0;  
+    
+    //hours
+    ctx.beginPath();
+    ctx.arc(250, 250, 200, 0, degToRad(180));
+    ctx.stroke();
+    
+    //minutes
+    ctx.beginPath();
+    ctx.arc(250, 250, 170, 0, degToRad(180));
+    ctx.stroke();
+    
+    //seconds
+    ctx.beginPath();
+    ctx.arc(250, 250, 140, 0, degToRad(180));
+    ctx.stroke();
     
 }
 
